72c466eb72fa935efc9ad4e7da827e00ba8594bd
Valen.perti <valentinoperticarari8@gmail.com>
2024-07-31 07:47:10 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,17 @@
+//let  significa q puede cambiar la variable
+//const significa q no va acambiar nunca
+
+const http = require("http")// './' accede al directorio dnd estas desp podes importarle alguna carpeta d ahi
+
+function requestController(){
+    //logica funcion
+    console.log({global});
+}
+
+//configurar nuestro sv
+//const server = http.createServer(requestController)
+
+//server.listen(4000)
+const person = {name :  "valen"};
+
+console.log(person.name);
